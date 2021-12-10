eebc45d47ddbd13e1f65221cccd2a7d5b714d2b6
maxProgrammer <maxwneto.poo@gmail.com>
2021-12-10 15:05:08 -0300

    criado exceção personalizada e levado todas as exceções para a classe Reservation.

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.excepctions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -16,10 +18,14 @@ public class Reservation {
 
 	}
 
-	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Error in reservation: Check-out date" + " must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
-		this.checkIn = checkin;
-		this.checkOut = checkout;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
 
 	public Integer getRoomNumber() {
@@ -43,20 +49,20 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public String updateDates(Date checkin, Date checkout) {
+	public void updateDates(Date checkIn, Date checkOut) {
 
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Error in reservation: Reservation dates " + "for update must be future dates.";
+			throw new DomainException(
+					"Error in reservation: Reservation dates " + "for update must be future dates.");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Error in reservation: Check-out date must be after check-in date";
+			throw new DomainException("Error in reservation: Check-out date" + " must be after check-in date");
 		}
 
-		this.checkIn = checkin;
-		this.checkOut = checkout;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 
-		return null;
 	}
 
 	@Override
